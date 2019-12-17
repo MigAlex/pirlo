8084fb879bd98169b5b5c708c4bc9aab5a82ab91
Alex <awangardowie@gmail.com>
2019-12-17 20:01:00 +0100

    146 - adding abstract AuthBase class

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -7,8 +7,14 @@ class User {
   final String uid; //user id
 }
 
+abstract class AuthBase{ //W146
+  Future<User> currentUser();
+  Future<User> signInAnonymously();
+  Future<void> signOut();
+}
+
 //W144
-class Auth{
+class Auth implements AuthBase{
 final _firebaseAuth = FirebaseAuth.instance;
 
 User _userFromFirebase(FirebaseUser user){ //metoda ta konwertuje Firebase object na obiekt user zawierajacy tylko uid
@@ -17,17 +23,19 @@ User _userFromFirebase(FirebaseUser user){ //metoda ta konwertuje Firebase objec
   }
   return User(uid: user.uid);
 }
-
+  @override
   Future<User> currentUser() async{
     final user =  await _firebaseAuth.currentUser();
     return _userFromFirebase(user);
   }
 
+  @override
   Future<User> signInAnonymously() async{
     final authResult = await _firebaseAuth.signInAnonymously();
     return _userFromFirebase(authResult.user);
   }
 
+  @override
   Future<void> signOut() async{
     await _firebaseAuth.signOut();
   }
