3501610d53beb7cde5a8e8455181ce1ef004f503
Alex <awangardowie@gmail.com>
2019-12-19 20:02:19 +0100

    WW177 - Facebook Sign In code - tyle ze mozliwe ze jakis permission zwalony, bo facebook sie niby laduje, ale zaraz znika

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class User {
@@ -15,6 +16,7 @@ abstract class AuthBase {
   Future<void> signOut();
   Stream<User> get onAuthStateChanged;
   Future<User> signInWithGoogle();
+  Future<User> signInWithFacebook();
 }
 
 //W144
@@ -51,6 +53,8 @@ class Auth implements AuthBase {
   Future<void> signOut() async {
     final googleSignIn = GoogleSignIn();
     await googleSignIn.signOut();
+    final facebookLogin = FacebookLogin();
+    await facebookLogin.logOut();
     await _firebaseAuth.signOut();
   }
 
@@ -76,6 +80,23 @@ class Auth implements AuthBase {
     } else {
       throw PlatformException(
           code: 'ERROR_ABORTED_BY_USER', message: 'Sign in aborted by user');
+    }
+  }
+
+  @override
+  Future<User> signInWithFacebook() async{
+    final facebookLogin = FacebookLogin();
+    final result = await facebookLogin.logInWithReadPermissions(['public_profile'],);
+    if(result.accessToken != null){
+      final authResult = await _firebaseAuth.signInWithCredential(
+        FacebookAuthProvider.getCredential(
+          accessToken: result.accessToken.token,
+        ),
+      );
+      return _userFromFirebase(authResult.user);
+    } else {
+      throw PlatformException(
+        code: 'ERROR_ABORTED_BY_USER', message: 'Sign in aborted by user');
     }
   }
 }
