debb93d791c17d4500b752d5d34845d4660ecded
Alex <awangardowie@gmail.com>
2019-12-21 19:55:12 +0100

    W194 - working on submit method

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -16,11 +16,22 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
 
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  String get _email => _emailController.text;
+  String get _password => _passwordController.text;
   EmailSignInFormType _formType = EmailSignInFormType.signIn;
 
-  void _submit() {
+  void _submit() async{
     print(
         'email: ${_emailController.text}, password: ${_passwordController.text}');
+    try{if(_formType == EmailSignInFormType.signIn){
+      await widget.auth.signInWithEmailAndPassword(_email, _password);
+    } else {
+      await widget.auth.createUserWithEmailAndPassword(_email, _password);
+    }
+    Navigator.of(context).pop();
+    } catch (e){
+      print(e.toString());
+    }
   }
 
   void _toggleFormType() { //przełączenie rodzaju formularza
