f391c8bfc25d354e22849f22bcbbb3ffde48a533
Alex <awangardowie@gmail.com>
2019-12-21 19:21:08 +0100

    W191 - stworzenie TextEditingController dla email_sign_in_form

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -2,16 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:rep_pirlo_1_dec/app/custom_widgets/form_submit_button.dart';
 
 class EmailSignInForm extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _submit(){
+    print('email: ${_emailController.text}, password: ${_passwordController.text}');
+  }
   List<Widget> _buildChildren() {
     return [
       TextField(
+        controller: _emailController,
         decoration:
             InputDecoration(labelText: 'Email', hintText: 'Elo@gmail.com'),
+
       ),
       SizedBox(
         height: 8,
       ),
       TextField(
+        controller: _passwordController,
         decoration: InputDecoration(labelText: 'Password'),
         obscureText: true,
       ),
@@ -20,7 +29,7 @@ class EmailSignInForm extends StatelessWidget {
       ),
       RaisedButton(
         child: Text('Sign in'),
-        onPressed: () {},
+        onPressed: _submit,
       ),
       SizedBox(
         height: 8,
