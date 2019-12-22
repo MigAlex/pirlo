4ce55f56ab1e3ec23c00c689b98a26fef8d02176
Alex <awangardowie@gmail.com>
2019-12-22 13:50:34 +0100

    W199 - adding new class + mixin

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:rep_pirlo_1_dec/app/custom_widgets/form_submit_button.dart';
+import 'package:rep_pirlo_1_dec/app/sign_in/validators.dart';
 import 'package:rep_pirlo_1_dec/services/auth.dart';
 
 enum EmailSignInFormType { signIn, register }
 
-class EmailSignInForm extends StatefulWidget {
+class EmailSignInForm extends StatefulWidget with EmailAndPasswordValidators {
   EmailSignInForm({@required this.auth});
   final AuthBase auth;
   @override
@@ -59,7 +60,8 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
         ? 'Need an account? Register!'
         : 'Have an account? Sign in';
 
-    bool submitEnabled = _email.isNotEmpty && _password.isNotEmpty;    
+    bool submitEnabled = widget.emailValidator.isValid(_email) && widget.passwordValidator.isValid(_password);
+      
     return [
       _buildEmailTextField(),
       SizedBox(
