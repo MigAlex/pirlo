dbf68fe441393b43a5ad1ffa9a84b8164c4ff243
Alex <awangardowie@gmail.com>
2020-03-31 15:35:38 +0200

    State management - Provider (S.16)

--- lib/app/sign_in/sign_in_manager.dart
+++ lib/app/sign_in/sign_in_manager.dart
@@ -3,24 +3,17 @@ import 'package:flutter/foundation.dart';
 import 'package:rep_pirlo_1_dec/services/auth.dart';
 
 class SignInBloc {
-  SignInBloc({@required this.auth});
+  SignInBloc({@required this.auth, @required this.isLoading});
   final AuthBase auth;
+  final ValueNotifier<bool> isLoading;
 
-  final StreamController<bool> _isLoadingController = StreamController<bool>();
-  Stream<bool> get isLoadingStream => _isLoadingController.stream;
-
-  void dispose() {
-    _isLoadingController.close();
-  }
-
-  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
 
   Future<User> _signIn(Future<User> Function() signInMethod) async {
     try {
-      _setIsLoading(true);
+      isLoading.value = true;
       return await signInMethod();
     } catch (e) {
-      _setIsLoading(false);
+      isLoading.value = false;
       rethrow;
     }
   }
