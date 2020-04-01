55a673c6a45f6a3c088abe9742487f792e5556fe
Alex <awangardowie@gmail.com>
2020-04-01 17:41:10 +0200

    303 - optimalization code

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -16,17 +16,22 @@ class FirestoreDatabase implements Database {
   Future<void> createJob(Job job) async =>
       await _setData(path: APIPath.job(uid, 'job_abc'), data: job.toMap());
 
-  Stream<List<Job>> jobsStream() {
-    final path = APIPath.jobs(uid);
-    final reference = Firestore.instance.collection(path);
-    final snapshots = reference.snapshots();
-    return snapshots.map((snapshot) => snapshot.documents.map(
-      (snapshot) => Job.fromMap(snapshot.data)).toList());
-  }
+  Stream<List<Job>> jobsStream() => _collectionStream(
+      path: APIPath.jobs(uid), builder: (data) => Job.fromMap(data));
 
   Future<void> _setData({String path, Map<String, dynamic> data}) async {
     final reference = Firestore.instance.document(path);
     print('$path: $data');
     await reference.setData(data);
+  }
+
+  Stream<List<T>> _collectionStream<T>({
+    @required String path,
+    @required T builder(Map<String, dynamic> data),
+  }) {
+    final reference = Firestore.instance.collection(path);
+    final snapshots = reference.snapshots();
+    return snapshots.map((snapshot) =>
+        snapshot.documents.map((snapshot) => builder(snapshot.data)).toList());
   }
 }
