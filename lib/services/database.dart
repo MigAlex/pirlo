9159a5b4c7035accaaf348081e8842815a668e22
Alex <awangardowie@gmail.com>
2020-04-01 14:04:56 +0200

    293

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:rep_pirlo_1_dec/app/home/models/job.dart';
+import 'package:rep_pirlo_1_dec/services/api_path.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
@@ -11,9 +12,14 @@ class FirestoreDatabase implements Database {
 
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
 
-  Future<void> createJob(Job job) async{
-    final path = '/users/$uid/jobs/job_abc';
-    final documentReference = Firestore.instance.document(path);
-    await documentReference.setData(job.toMap()); //returns future
+  Future<void> createJob(Job job) async => await _setData(
+    path: APIPath.job(uid, 'job_abc'),
+    data: job.toMap()
+  );
+
+  Future<void> _setData({String path, Map<String, dynamic> data}) async {
+    final reference = Firestore.instance.document(path);
+    print('$path: $data');
+    await reference.setData(data);
   }
 }
