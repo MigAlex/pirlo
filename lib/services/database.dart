bd21deaa9304d1e902b81b778ad3403635243595
Alex <awangardowie@gmail.com>
2020-04-01 14:36:46 +0200

    297

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -5,6 +5,7 @@ import 'package:rep_pirlo_1_dec/services/api_path.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
+  void readJobs();
 }
 
 class FirestoreDatabase implements Database {
@@ -12,10 +13,17 @@ class FirestoreDatabase implements Database {
 
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
 
-  Future<void> createJob(Job job) async => await _setData(
-    path: APIPath.job(uid, 'job_abc'),
-    data: job.toMap()
-  );
+  Future<void> createJob(Job job) async =>
+      await _setData(path: APIPath.job(uid, 'job_abc'), data: job.toMap());
+
+  void readJobs() {
+    final path = APIPath.jobs(uid);
+    final reference = Firestore.instance.collection(path);
+    final snapshots = reference.snapshots();
+    snapshots.listen((snapshot) { //przeglada all documents w kolekcji i nastepnie printuje dane zawarte w snapshocie tego document, argument snapshot w tej metodzie to snapshot kolekcji przetrzymującej listę documentów
+      snapshot.documents.forEach((snapshot) => print(snapshot.data));
+    });
+  }
 
   Future<void> _setData({String path, Map<String, dynamic> data}) async {
     final reference = Firestore.instance.document(path);
