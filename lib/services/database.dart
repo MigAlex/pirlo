c732294de69899def0cb2c969d4eff4540bc7eb0
Alex <awangardowie@gmail.com>
2020-04-01 17:53:10 +0200

    304 - creating firestore service

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,7 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:rep_pirlo_1_dec/app/home/models/job.dart';
 import 'package:rep_pirlo_1_dec/services/api_path.dart';
+import 'package:rep_pirlo_1_dec/services/firestore_service.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
@@ -13,25 +13,11 @@ class FirestoreDatabase implements Database {
 
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
 
-  Future<void> createJob(Job job) async =>
-      await _setData(path: APIPath.job(uid, 'job_abc'), data: job.toMap());
+  final _service = FireStoreService.instance;
 
-  Stream<List<Job>> jobsStream() => _collectionStream(
+  Future<void> createJob(Job job) async => await _service.setData(
+      path: APIPath.job(uid, 'job_abc'), data: job.toMap());
+
+  Stream<List<Job>> jobsStream() => _service.collectionStream(
       path: APIPath.jobs(uid), builder: (data) => Job.fromMap(data));
-
-  Future<void> _setData({String path, Map<String, dynamic> data}) async {
-    final reference = Firestore.instance.document(path);
-    print('$path: $data');
-    await reference.setData(data);
-  }
-
-  Stream<List<T>> _collectionStream<T>({
-    @required String path,
-    @required T builder(Map<String, dynamic> data),
-  }) {
-    final reference = Firestore.instance.collection(path);
-    final snapshots = reference.snapshots();
-    return snapshots.map((snapshot) =>
-        snapshot.documents.map((snapshot) => builder(snapshot.data)).toList());
-  }
 }
