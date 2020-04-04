36aea5731bfdfc5b32c7fa2128c345438624851b
Alex <awangardowie@gmail.com>
2020-04-04 21:22:23 +0200

    311

--- lib/app/home/jobs/add_job_page.dart
+++ lib/app/home/jobs/add_job_page.dart
@@ -13,12 +13,37 @@ class AddJobPage extends StatefulWidget {
 }
 
 class _AddJobPageState extends State<AddJobPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _name;
+  int _ratePerHour;
+
+  bool _validateAndSaveForm(){
+    final form = _formKey.currentState;
+    if(form.validate()){
+      form.save();
+      return true;
+    }
+    return false;
+  }
+  void _submit() {
+    if(_validateAndSaveForm()){
+      print('form saved: $_name, $_ratePerHour');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 2,
         title: Text('New Job'),
+        actions: <Widget>[
+          FlatButton(
+              onPressed: _submit,
+              child: Text('Save',
+                  style: TextStyle(fontSize: 18, color: Colors.white)))
+        ],
       ),
       body: _buildContents(),
       backgroundColor: Colors.grey[200],
@@ -41,6 +66,7 @@ class _AddJobPageState extends State<AddJobPage> {
 
   Widget _buildForm() {
     return Form(
+      key: _formKey,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: _buildFormChildren(),
@@ -52,12 +78,15 @@ class _AddJobPageState extends State<AddJobPage> {
     return [
       TextFormField(
         decoration: InputDecoration(labelText: 'Job Name'),
+        onSaved: (value) => _name = value,
+        validator: (value) => value.isNotEmpty ? null : 'Name cannot be empty',
       ),
-      TextField(
-        decoration: InputDecoration(labelText: 'Rate per hour'),
+      TextFormField(
+        decoration:  InputDecoration(labelText: 'Rate per hour'),
+        onSaved: (value) => _ratePerHour = int.tryParse(value),
         keyboardType:
             TextInputType.numberWithOptions(signed: false, decimal: false),
-      )
+      ),
     ];
   }
 }
